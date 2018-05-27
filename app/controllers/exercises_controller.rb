0ff6d811b33c91cf767470009768902f1ac48c41
fcalderon <fcalderon@nearsoft.com>
2018-05-26 18:43:24 -0600

    Add create functionality

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -6,4 +6,20 @@ class ExercisesController < ApplicationController
     def new
         @exercise = Exercise.new
     end
+
+    def create
+        @exercise = Exercise.new(exercise_params)
+
+        if @exercise.save
+            redirect_to exercises_path, notice: t('exercise.exercise-successfuly-created')
+        else
+            redirect_to new_exercise_path, notice: t('exercise.errors.exercise-not-created')
+        end
+    end
+
+    private
+    
+    def exercise_params
+        params.require(:exercise).permit(:name, :body_part)
+    end
 end
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,10 @@
 class Exercise < ApplicationRecord
+    before_save :normalize_attributes
+    validates_presence_of :name, :body_part
+
+    private
+    def normalize_attributes
+        self.name = self.name.humanize.titleize
+        self.body_part = self.body_part.humanize.titleize
+    end
 end
